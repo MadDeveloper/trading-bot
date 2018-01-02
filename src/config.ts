9c210d6f094c47015783d30b3c0cee72b2e8dac5
MadDeveloper <sergent.julien@icloud.com>
2018-01-02 20:11:31 +0100

    Starting code...

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+const config = {
+    api: {
+        // Permissions: view, trade, manage, transfer, bypass-2fa
+        secret: 'SAMGMhUf2soybuUCGW1kNawZaA97eZIkmCP3FJ+THMZcuErC35AIk05VAz68mCXzkvmlJmYodUvbA+oP3eE8sQ==',
+        key: '95cb265defffd916cbc28628dc294c56',
+        passphrase: 'IOnGpg7qyS',
+        uri: 'https://api.gdax.com',
+        websocketURI: 'wss://ws-feed.gdax.com',
+        sandboxURI: 'https://api-public.sandbox.gdax.com'
+    }
+}
+
+export default config
